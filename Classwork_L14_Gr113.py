9de155d5ddcdcaa0a051bc38508be93c85ae3403
LikaSv <45724202+LikaSv@users.noreply.github.com>
2023-12-29 18:41:13 +0400

    Add files via upload
    avtvvirtef dh df hdfh

---
+++ Classwork_L14_Gr113.py
@@ -0,0 +1,27 @@
+# text = 'python php pascal javascript java c++'
+# pr_list = text.split()
+# length_list = [len(i) for i in pr_list]
+# max_ind = length_list.index(max(length_list))
+# print('მაქსიმალური სიგრძისაა: ', pr_list[max_ind])
+
+
+# import my_module
+# from my_module import avg
+# print(avg(3, 6))
+
+# import pkg.mod2
+# print(pkg.mod2.jami(3, 6))
+
+
+# from pkg  import mod2
+# print(mod2.jami(3, 6))
+
+# from pkg.mod2 import *
+# print(jami(3, 6))
+
+from dateutil.easter import *
+print(easter(2024, 2))
+
+
+
+
